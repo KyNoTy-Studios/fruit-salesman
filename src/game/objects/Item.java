57bf71307d9d37fb63544ac9e137de37f1af69f1
Zvonty Flugence <zvontyflugence@gmail.com>
2018-11-16 15:35:33 -0500

    Expanded on Item Interface, Create Apple Item

--- src/game/objects/Item.java
+++ src/game/objects/Item.java
@@ -3,32 +3,53 @@ package game.objects;
 public interface Item {
 
     /**
-     *
+     * Returns the id of the item.
+     * @return the item's id as an int
      */
+    int getId();
 
     /**
-     * Returns the name of the item
-     *
+     * Returns the name of the item.
      * @return the item's name as a String
-     * @since 1.10
      */
     String getName();
 
     /**
-     * Returns the price of the item
-     *
+     * Returns the price of the item.
      * @return the item's price as a long
-     * @since 1.10
      */
     long getPrice();
 
     /**
-     * Retuns the sprite of the item
-     *
+     * Retuns the sprite of the item.
      * @return the item's sprite as a BufferedImage
-     * @since 1.10
      */
     int[] getSprite();
+
+    /**
+     * Returns item's current row position on the game map.
+     * @return the item's current map row position (-1 means the item has not been placed)
+     */
+    int getX();
+
+    /**
+     * Returns item's current column position on the game map.
+     * @return the item's current map column position (-1 means the item has not been placed)
+     */
+    int getY();
+
+    /**
+     * Sets the item's current map position.
+     * @param x target row position
+     * @param y target column position
+     */
+    void setPos(int x, int y);
+
+    /**
+     * Clears the item's current map position.
+     * Sets item's x and y values to -1 (invalid position);
+     */
+    void clearPos();
 
 
 }
---
+++ src/game/objects/fruit/Apple.java
@@ -0,0 +1,53 @@
+package game.objects.fruit;
+
+public class Apple implements Item {
+    private static final int ID = 3;
+    private static final String NAME = "Apple";
+    private long price;
+    private int[] pixels;
+    private int x, y;
+
+    public Apple(long price) {
+        this(price, -1, -1)
+    }
+
+    public Apple(long price, int x, int y) {
+        this.price = price;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getId() {
+        return ID;
+    }
+
+    public String getName() {
+        return NAME;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public int[] getSprite() {
+        return pixels;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public setPos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public clearPos() {
+        x = -1;
+        y = -1;
+    }
+}
